39c999e9d7fbb617257cd4e1ed2390f555d10d80
MenachemZeivald <108309381+MenachemZeivald@users.noreply.github.com>
2022-11-16 12:22:32 +0200

    first commit

---
+++ src/script.js
@@ -0,0 +1,13 @@
+// let hrs = document.querySelector('.hrs-line')
+// let min = document.querySelector('.min-line')
+// let sec = document.querySelector('.sec-line')
+// let minute = 360 / 60;
+// hrs.style.animationPlayState = 'paused';
+// min.style.animationPlayState = 'paused';
+// sec.style.animationPlayState = 'paused';
+// setInterval(() => {
+//     hrs.style.transform = 'rotate(' + (((Date().slice(16, 18) % 12)*60) + 1 * Date().slice(19, 21)) * (360 / (12 * 60)) + 'deg)'
+//     sec.style.transform = 'rotate(' + (Date().slice(22, 24) * minute) + 'deg)' // (ate().slice(19, 21) * 60 + 1 * Date().slice(22, 24) * (360 / 60)
+//     min.style.transform = 'rotate(' + (Date().slice(19, 21) * minute) + 'deg)'
+// }, 1000);
+
