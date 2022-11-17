51ee8240d8a1ab5ccc367f576df66728504f3993
MenachemZeivald <108309381+MenachemZeivald@users.noreply.github.com>
2022-11-17 14:29:29 +0200

    animate the time now

--- src/script.js
+++ src/script.js
@@ -1,13 +1,9 @@
-// let hrs = document.querySelector('.hrs-line')
-// let min = document.querySelector('.min-line')
-// let sec = document.querySelector('.sec-line')
-// let minute = 360 / 60;
-// hrs.style.animationPlayState = 'paused';
-// min.style.animationPlayState = 'paused';
-// sec.style.animationPlayState = 'paused';
-// setInterval(() => {
-//     hrs.style.transform = 'rotate(' + (((Date().slice(16, 18) % 12)*60) + 1 * Date().slice(19, 21)) * (360 / (12 * 60)) + 'deg)'
-//     sec.style.transform = 'rotate(' + (Date().slice(22, 24) * minute) + 'deg)' // (ate().slice(19, 21) * 60 + 1 * Date().slice(22, 24) * (360 / 60)
-//     min.style.transform = 'rotate(' + (Date().slice(19, 21) * minute) + 'deg)'
-// }, 1000);
+let hrs = document.querySelector('.hrs-line-container')
+let min = document.querySelector('.min-line-container')
+let sec = document.querySelector('.sec-line-container')
+let minute = 360 / 60;
 
+// create initial state
+hrs.style.transform = 'rotate(' + (((Date().slice(16, 18) % 12)*60) + 1 * Date().slice(19, 21)) * (360 / (12 * 60)) + 'deg)'
+sec.style.transform = 'rotate(' + (Date().slice(22, 24) * minute) + 'deg)'
+min.style.transform = 'rotate(' + (Date().slice(19, 21) * minute) + 'deg)'
